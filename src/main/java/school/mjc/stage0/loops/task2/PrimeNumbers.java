bd80894911bcc1fe2f0c3ec9294a93820c04a00a
Farrukh_Rajabov <Farruhrajabov2012@gmail.com>
2024-07-05 07:58:44 +0500

    Initial commit

--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,5 +2,20 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
+        int counter = 0;
+        while (counter <= printToInclusive) {
+            int divider = 1;
+            int sum = 0;
+            while (divider < counter) {
+                if (counter % divider == 0) {
+                    sum++;
+                }
+                divider++;
+            }
+            if (sum > 0) {
+                System.out.println(counter);
+            }
+            counter++;
+        }
     }
 }
